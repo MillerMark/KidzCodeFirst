96bd65283b103433e4c5625b2458e030cff4b3e9
Mark003 <coderush@gmail.com>
2020-11-07 10:27:09 -0600

    Refactored code to move BlowUpBlock to Obstacle Logic.

--- Assets/MissileBehavior.cs
+++ Assets/MissileBehavior.cs
@@ -33,25 +33,22 @@ public class MissileBehavior : MonoBehaviour
 		}
 	}
 
-	void BlowUpBlock(GameObject gameObject)
+	void OnCollisionEnter(Collision collision)
 	{
-		const int numBlocksPerSide = 2;
-		const float scale = 1f / numBlocksPerSide;
-		Vector3 blockPosition = gameObject.transform.position;
-		float localScale = scale * gameObject.transform.localScale.x;
-		Vector3 scaleVector = new Vector3(localScale, localScale, localScale);
-
-		Vector3 positionVector = new Vector3(blockPosition.x, blockPosition.y, blockPosition.z);
-
-		for (int i = 0; i < Math.Pow(numBlocksPerSide, 3); i++)
+		if (!collision.gameObject.name.Contains("PrototypeBlock"))
 		{
-			CreateParticle(gameObject, scaleVector, positionVector);
+			//Debug.Log("Only blowing up blocks!!!");
+			return;
 		}
-
-		Destroy(gameObject);
-
+		ObstacleLogic.BlowUpBlock(collision.gameObject, BlackHole);
 		numBlocksDestroyed++;
 
+		CheckForPowerUp();
+		Destroy(gameObject);
+	}
+
+	private void CheckForPowerUp()
+	{
 		if (numBlocksDestroyed >= numBlocksToPowerUp)
 		{
 			const float dropAheadPosition = 19f;
@@ -62,26 +59,5 @@ public class MissileBehavior : MonoBehaviour
 			rigidbody.useGravity = true;
 			numBlocksDestroyed = 0;
 		}
-	}
-
-	private void CreateParticle(GameObject gameObject, Vector3 scaleVector, Vector3 positionVector)
-	{
-		GameObject newParticle = Instantiate(gameObject, positionVector, Quaternion.identity);
-		newParticle.transform.localScale = scaleVector;
-		Rigidbody rigidbody = newParticle.GetComponent<Rigidbody>();
-		rigidbody.useGravity = false;
-		FloatingBehavior floatingBehavior = newParticle.AddComponent<FloatingBehavior>();
-		floatingBehavior.BlackHole = BlackHole;
-	}
-
-	void OnCollisionEnter(Collision collision)
-	{
-		if (!collision.gameObject.name.Contains("PrototypeBlock"))
-		{
-			//Debug.Log("Only blowing up blocks!!!");
-			return;
-		}
-		BlowUpBlock(collision.gameObject);
-		Destroy(gameObject);
 	}
 }
--- Assets/ObstacleLogic.cs
+++ Assets/ObstacleLogic.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -26,5 +27,33 @@ public class ObstacleLogic : MonoBehaviour
 			floatingBehavior.BlackHole = BlackHole;
 			isFloating = true;
 		}
+	}
+
+	public static void BlowUpBlock(GameObject gameObject, Transform blackHole)
+	{
+		const int numBlocksPerSide = 2;
+		const float scale = 1f / numBlocksPerSide;
+		Vector3 blockPosition = gameObject.transform.position;
+		float localScale = scale * gameObject.transform.localScale.x;
+		Vector3 scaleVector = new Vector3(localScale, localScale, localScale);
+
+		Vector3 positionVector = new Vector3(blockPosition.x, blockPosition.y, blockPosition.z);
+
+		for (int i = 0; i < Math.Pow(numBlocksPerSide, 3); i++)
+		{
+			CreateParticle(gameObject, scaleVector, positionVector, blackHole);
+		}
+
+		Destroy(gameObject);
+	}
+
+	private static void CreateParticle(GameObject gameObject, Vector3 scaleVector, Vector3 positionVector, Transform blackHole)
+	{
+		GameObject newParticle = Instantiate(gameObject, positionVector, Quaternion.identity);
+		newParticle.transform.localScale = scaleVector;
+		Rigidbody rigidbody = newParticle.GetComponent<Rigidbody>();
+		rigidbody.useGravity = false;
+		FloatingBehavior floatingBehavior = newParticle.AddComponent<FloatingBehavior>();
+		floatingBehavior.BlackHole = blackHole;
 	}
 }
